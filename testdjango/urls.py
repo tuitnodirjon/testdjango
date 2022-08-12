c59cd39db2c44f8be01913217638e495df44c944
nodirjon <n.ruzimurodov@gmail.com>
2022-08-12 20:11:16 +0500

    nodir

--- testdjango/urls.py
+++ testdjango/urls.py
@@ -3,7 +3,14 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
-Function views
+Function viewssfasd
+
+a
+sd
+as
+da
+sd
+asd
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
